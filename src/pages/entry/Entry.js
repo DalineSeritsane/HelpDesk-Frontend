413b4ae220b203344b71117b6a7d10a5dde01a7c
DalineSeritsane <dalinelerato@gmail.com>
2025-06-11 06:32:08 +0200

    login comp

---
+++ src/pages/entry/Entry.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./entry.css";
+import Login from "../../components/login/Login"
+
+const Entry = () => {
+ return (
+  <div>
+    <div className='entry-page  '>entry page</div>
+    <Login />
+  </div>
+)
+
+}
+
+export default Entry
+
