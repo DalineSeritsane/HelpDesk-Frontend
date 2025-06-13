e865640fdbca83350396fad609c4ab6d1784aa7e
DalineSeritsane <dalinelerato@gmail.com>
2025-06-14 01:29:14 +0200

    dashboard added with dummy tickets tbale

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Entry from "./pages/entry/Entry";
 import DefaultLayout from "./layout/DefaultLayout";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
      {/* <Entry /> */}
-     <DefaultLayout > //Dashboard </DefaultLayout> 
+     <DefaultLayout > <Dashboard /> </DefaultLayout> 
     </div>
   );
 }
